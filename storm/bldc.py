7cde58eacc5e541f0612f53a528e49509a4bbeee
Kevin Strasser <kevin.strasser@linux.intel.com>
2012-09-26 13:51:04 -0700

    bldc: adding enough code to start testing OO

--- storm/bldc.py
+++ storm/bldc.py
@@ -14,13 +14,16 @@ from robovero.lpc_types import FunctionalState
 
 from robovero.arduino import analogWrite, PWM1
 
+from sys import stdout
+# Entry Point
+roboveroConfig()
+initPWM()	
 
-def getServoAngle():
-	"""Get an angle from the user and calculate new duty cycle.
+def getServoAngle(self, raw_angle):
+	"""Get an angle and calculate new duty cycle.
 	"""
-	user_angle = raw_input("New angle: ")
 	try:
-		angle = int(user_angle)
+		angle = int(raw_angle)
 		if angle < 0 or angle > 180:
 			raise InputError
 	except:
@@ -29,35 +32,60 @@ def getServoAngle():
 	match_value = 1100 + (angle*500/180)
 	return match_value
 
-def initPulse(channel, pulse_width):
-	initMatch(channel, pulse_width)
-	
-def initPeriod(period):
-	initMatch(0, period)
+class BLDC():
 
-def initPWM():
-	"""Set up PWM to output a 1.5ms pulse at 50Hz.
-	"""
+	def __init__(self, period=20000):
+		"""Set up PWM at 50Hz.
+		"""
 
-	# Set the period to 20000us = 20ms = 50Hz
-	initPeriod(20000)
+		# Set the period to 20000us = 20ms = 50Hz
+		initMatch(0, period)
 
-	# Set the pulse width to 1.5ms
-	initPulse(1, 1500)
-	
-	PWM_ChannelCmd(LPC_PWM1, 1, FunctionalState.ENABLE)
-	PWM_ResetCounter(LPC_PWM1)
-	PWM_CounterCmd(LPC_PWM1, FunctionalState.ENABLE)
-	PWM_Cmd(LPC_PWM1, FunctionalState.ENABLE)
+		PWM_ResetCounter(LPC_PWM1)
+		PWM_CounterCmd(LPC_PWM1, FunctionalState.ENABLE)
+		PWM_Cmd(LPC_PWM1, FunctionalState.ENABLE)
 
-# Entry Point
-roboveroConfig()
-initPWM()	
+		self.channels = []
+		stdout.write("Initialized BLDC motor controller")	
 
-while True:
-	match_value = getServoAngle()
-	if match_value:
-		PWM_MatchUpdate(LPC_PWM1, 1, match_value, PWM_MATCH_UPDATE_OPT.PWM_MATCH_UPDATE_NOW)
+	def add_channel(self, raw_channel, init_pulse = 1500):
+		"""Add PWM channel with a 1.5ms pulse.
+		"""
+		try:
+			channel = int(raw_channel)
+			if channel < 1 or channel > 5:
+				raise InputError
+		except:
+			stdout.write("%s is an invalid channel.\n" % raw_channel)
+			return None 
 		
+		# Check if channel has already been added
+		if channel in self.channels:
+			stdout.write("Channel %d has already been initialized.\n" % channel)
+			return None
 
+		self.channels.append(channel)
 
+		initMatch(channel, init_pulse)
+		PWM_ChannelCmd(LPC_PWM1, channel, FunctionalState.ENABLE)
+		#TODO: might need counter commands here. check this first.
+
+		stdout.write("Initializing BLDC pwm channel %d." % channel)
+
+	def set_pulse(self, channel, pulse):
+		
+		if channel not in self.channels:
+			stdout.write("%s is invalid or is not an initialized channel.\n" % channel)
+			return None
+		
+		
+		PWM_MatchUpdate(LPC_PWM1, channel, pulse, PWM_MATCH_UPDATE_OPT.PWM_MATCH_UPDATE_NOW)
+
+if __name__ == "__main__":
+
+	motor = BLDC()
+	motor.add_channel(1)
+	while True:
+		match_value = getServoAngle()
+		if match_value:
+			motor.set_pulse(match_value)	
